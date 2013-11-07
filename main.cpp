d6413a9797f2c47b0e3a19ab9b4fef2d15d58603
Sanket Sudake <sanketsudake@gmail.com>
2013-11-07 19:12:04 +0530

    Enable Texture mapping, fix errors

--- main.cpp
+++ main.cpp
@@ -1,66 +1,96 @@
+/*
+ * Filename: main.cpp
+ * Author: Sanket Sudake <sanketsudake at gmail.com>
+ * Licence: GNU GPL v3
+ */
+
 using namespace std;
 
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+// SDL Dev header file
 #include <SDL/SDL.h>
 #include <SDL/SDL_image.h>
+// OpenGL header files
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 
+// Load windowid for current window
 int windowid = 0;
+
 unsigned int bg = 0;
 
+/*
+ * Load texture image with specified name
+ * prints error and exists program if specified image
+ * not working properly
+ * return texture image id
+ */
 unsigned int loadTexture(const char* filename)
 {
 	unsigned int id;
 	SDL_Surface* img = SDL_LoadBMP(filename);
+
 	if(img == 0)
 	{
-		printf("Binding error");
+		printf("\nBinding error .\n");
 		exit(1);
 	}
 	glGenTextures(1, &id);
 	glBindTexture(GL_TEXTURE_2D, id);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, img->w, img->h, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, img->pixels);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, img->w, img->h, 0, GL_RGB, GL_UNSIGNED_SHORT_5_6_5, img->pixels);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	SDL_FreeSurface(img);
 	return id;
 }
+
+/*
+ * Innitialize all parameters for opengl window
+ * Enable all required features
+ */
 static void init()
 {
-	/*
-	 *Innitialize OpenGL
-	 */
 	glClearColor(0, 0, 0, 0);
 	glMatrixMode(GL_PROJECTION);
 	glOrtho(0, 10, 0, 10, 0, 10);
-	bg =  loadTexture("castle.bmp");
-}
-static void display()
-{
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glEnable(GL_DEPTH);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_BLEND);
+	glEnable(GL_TEXTURE_2D);
+
+	bg =  loadTexture("assets/bg2.bmp");
+}
+
+/*
+ * Display routine for window
+ */
+static void display()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-	glColor4f(1, 0, 0, 1);
+//	glColor4f(1, 0, 0, 0.5);
+	// Bind texture according to ortho parameters
 	glBindTexture(GL_TEXTURE_2D, bg);
 	glBegin(GL_QUADS);
 	glTexCoord2f(0.0, 1.0);
 	glVertex3f(0, 0, 0.0);
 	glTexCoord2f(0.0, 0.0);
-	glVertex3f(0, 80, 0.0);
+	glVertex3f(0, 90, 0.0);
 	glTexCoord2f(1.0,  0.0);
-	glVertex3f(150, 80, 0.0);
+	glVertex3f(160, 90, 0.0);
 	glTexCoord2f(1.0, 1.0);
-	glVertex3f(150, 0, 0.0);
+	glVertex3f(160, 0, 0.0);
 	glEnd();
 
 	glutSwapBuffers();
 }
+
+/*
+ * Reshapes window when maximized
+ */
 static void reshape(int w, int h)
 {
 	glViewport(0, 0, w, h);
@@ -68,6 +98,8 @@ static void reshape(int w, int h)
 	glLoadIdentity();
 	glOrtho(0, 160, 0, 90, 0, 100);
 }
+
+
 static void keyboard(unsigned char key, int x, int y)
 {
 	/*
@@ -92,9 +124,10 @@ static void keyboard(unsigned char key, int x, int y)
 
 int main(int argc, char *argv[])
 {
-    glutInit(&argc, argv);
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(900, 700);
 	glutCreateWindow("RainGL");
 	windowid = glutGetWindow();
 	init();
@@ -103,5 +136,5 @@ int main(int argc, char *argv[])
 	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	glutMainLoop();
-    return 0;
+	return 0;
 }
